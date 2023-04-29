ff8a2ab16dd052afd0544d218d4eceab6bf9e8c3
Masum Rahman <masumrahman73@gmail.com>
2023-04-29 22:00:06 +0600

    M62 header and right nav done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYzdfnWVM7ETYubLbFoSQITZlxeQthIhI",
+  authDomain: "the-news-dragon-254df.firebaseapp.com",
+  projectId: "the-news-dragon-254df",
+  storageBucket: "the-news-dragon-254df.appspot.com",
+  messagingSenderId: "783725048276",
+  appId: "1:783725048276:web:21a22301905c155d76e641"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
