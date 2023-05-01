7e9e8f954133e7d63be59d4daaba733aabf1e962
Masum Rahman <masumrahman73@gmail.com>
2023-05-01 15:26:25 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('env variable', import.meta.env.VITE_PASS);
 const firebaseConfig = {
-  apiKey: "AIzaSyAYzdfnWVM7ETYubLbFoSQITZlxeQthIhI",
-  authDomain: "the-news-dragon-254df.firebaseapp.com",
-  projectId: "the-news-dragon-254df",
-  storageBucket: "the-news-dragon-254df.appspot.com",
-  messagingSenderId: "783725048276",
-  appId: "1:783725048276:web:21a22301905c155d76e641"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
